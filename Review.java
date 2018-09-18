85c22ce5bd58586c02bd530c14f8eec2944e6a8c
I324094 <natalie.zhang01@sap.com>
2018-09-18 11:54:28 +0800

    2nd edition

--- Review.java
+++ Review.java
@@ -2,17 +2,14 @@
 public class Review {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Review myReview = new Review();
-		myReview.exampleMethod("Print this string");
-	}
-
-	public static void exampleStatic () {
-		System.out.println("Example Static");
-	}
-	
-	public String exampleMethod (String message) {
-//		System.out.println(message);
-		return message;
+		String[] breakfast = new String[3];
+		
+		breakfast[0] = "Bacon";
+		breakfast[1] = "Eggs";
+		breakfast[2] = "Toast";
+		
+		for(int i = 0; i<breakfast.length; i++) {
+			System.out.println(breakfast[i]);
+		}
 	}
 }
