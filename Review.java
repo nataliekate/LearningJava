41e958cb6466c56fc3828cb151406bee2b03763b
I324094 <natalie.zhang01@sap.com>
2018-09-18 12:11:30 +0800

    Input and output

--- Review.java
+++ Review.java
@@ -1,10 +1,17 @@
+import java.util.Scanner;
 
 public class Review {
 
 	public static void main(String[] args) {
-		MyClass mc = new MyClass();
 		
-		System.out.println(mc.myNum);
-		mc.myMethod();
+		System.out.println("Enter the first number: ");
+		Scanner scan = new Scanner(System.in);
+		int num1 = scan.nextInt();
+		
+		System.out.println("Enter the second number: ");
+		
+		int num2  = scan.nextInt();
+		
+		System.out.println("Sum is " + (num1 + num2));
 	}
 }
